ffb77318a1a22a44a9a2892b32820f4a6d8c6f59
superagurk <superagurken@gmail.com>
2014-04-07 13:44:10 +0200

    pasient
    tatt bort æøå

--- Source/Pasient/Pasient.java
+++ Source/Pasient/Pasient.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package pasient;
 
 /*
@@ -28,7 +22,7 @@ public class Pasient implements Serializable
 	  {
 		navn = n;
 		fdato = fd;
-		//liste over resepter skrevet ut på pasienten.
+		//liste over resepter skrevet ut paa pasienten.
 
 	  }
 
