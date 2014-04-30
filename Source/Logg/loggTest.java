17d4d62f6a9a625005d7032f709f5b85092f2985
superagurk <superagurken@gmail.com>
2014-04-30 14:17:30 +0200

    Forbedret logg
    lagrer nå alt som skrives inn i loggen

---
+++ Source/Logg/loggTest.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author Ole
+ */
+public class loggTest {
+    	public static void main (String [] args) {
+
+		Logg test = new Logg();
+                System.out.printf(test.toString("hei igjen"));
+	}
+
+
+}
