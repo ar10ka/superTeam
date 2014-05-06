01a8067cef60b8bb7d093b7ceeec5c42736ba0e8
M4R1US <baltr.marius@gmail.com>
2014-05-06 12:29:53 +0200

    DesignTest
    DesignTest

--- Source/Lege/DesignTest.java
+++ Source/Lege/DesignTest.java
@@ -1,11 +1,13 @@
 import java.awt.BorderLayout;
 import java.util.*;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.plaf.ColorUIResource;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -17,50 +19,50 @@ import java.util.ArrayList;
 public class DesignTest extends JFrame{
 	
 	private JFrame f;
-	private JPanel p;
 	
 	private JLabel l;
-	private Border border;
 	private JToolBar toolbar;
 	private Lytter sensor;
 	//private List<JButton> knappeListe = new ArrayList<>();
 	private JButton[] knappeListe;
 	private toolBarKnapper knapper;
+	private JPanel legeRegisterPanel;
+	//private final int x = 4;
 	
 
 	
 	public DesignTest() {
+		f = new JFrame("Design-test");
 		knapper = new toolBarKnapper();
 		sensor = new Lytter();
 		knappeListe = knapper.returnerObjektet();
 		toolbar();
+		f.getContentPane().setBackground(new Color(102,102,255));
+		f.setSize(1000,500);
+		f.setVisible(true);
 
 	}
-	
-	
-	/*public int selectedKnappen() {
-		
-		for( int i = 0; i <= knappeListe.length; i++) {
-			if(knappeListe[i].isSelected()) {
-				return i;
-			}
-		}
-		return -1;
-	} */
-	
+
 
 	public void toolbar() {
 		
-
-		f = new JFrame("Design-test");
-		p = new JPanel();
-		f.setSize(1000,500);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		toolbar =  new JToolBar();
+		//Border nedreLinja = BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(0,0,0));
+		Border utenLinjer = BorderFactory.createEmptyBorder();
+		//f.setUndecorated( true );
+		//f.getRootPane().setWindowDecorationStyle(x);
+		//f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		toolbar =  new JToolBar();		
+		toolbar.setLayout(new GridLayout());
+		toolbar.setBorder(utenLinjer);
 		
-		
-		p.setLayout(new GridLayout());
-		f.add(toolbar, BorderLayout.NORTH);
+		 legeRegisterPanel = new JPanel ();
+		 legeRegisterPanel.setSize(20,20);
+		 legeRegisterPanel.setBackground(Color.red);
+		 legeRegisterPanel.setVisible(true);
+		 
+		//JFrame.setDefaultLookAndFeelDecorated(true);  
+		f.add(toolbar,BorderLayout.NORTH);	 
+		//f.add(legeRegisterLabel, BorderLayout.NORTH);
 		
 		
 		for(JButton x: knappeListe) {
@@ -69,27 +71,30 @@ public class DesignTest extends JFrame{
 		}
 		
 
-		toolbar.setBackground(Color.GRAY);
-		f.setVisible(true);
+		toolbar.setBackground(new Color(128,128,228));
+		
 		toolbar.setFloatable(false);
 		
 		toolbar.setVisible(true);
-
 		
 	}
 	
-	  private class Lytter implements ActionListener {
+	 private class Lytter implements ActionListener {
 		    @Override
+		    
 		    
 		    public void actionPerformed(ActionEvent e)
 		    {
 		    	
+		    	Border sidelinjer = BorderFactory.createMatteBorder(0, 1, 0, 1, new Color(112,112,112));
+		    	Border utenlinjer = BorderFactory.createEmptyBorder();
 		    	
 		    	
 		    	for(int i = 0; i < knappeListe.length; i++) {
 
 		    		if(e.getSource() == knappeListe[i]) {
 		    			if(i == 0)
+		    				
 	    					System.out.println("LegeRegister");
 		    			if(i == 1)
 	    					System.out.println("2");
@@ -102,56 +107,20 @@ public class DesignTest extends JFrame{
 		    			if(i == 5)
 	    					System.out.println("6");
 		    			
-		    			knappeListe[i].setBackground(new Color(200,200,200));
-		    			
+		    			knappeListe[i].setBackground(new Color(102,102,255));
+		    			knappeListe[i].setBorder(sidelinjer);
+
 		    		}
-		    		else knappeListe[i].setBackground(new Color(160,160,160));
-		    				
+		    		else  { knappeListe[i].setBackground(new Color(128,128,128));
+		    				knappeListe[i].setBorder(utenlinjer); }
 		    				
 		    	}
 
-		    	
-		    	
-		    	
-		    	/*
-		    	if(e.getSource() == knappeListe[0]) {
-		    		System.out.println("LegeRegister");
-		    		knappeListe[0].setBackground(Color.blue);
-		    	}  else  knappeListe[0].setBackground(Color.lightGray); 
-		    	
-		    	
-		    	if(e.getSource() == knappeListe[1] ) {
-		    		System.out.println("PasientRegister");
-		    		knappeListe[1].setBackground(Color.blue);
-		    	} else  knappeListe[1].setBackground(Color.lightGray); 
-		    	
-		    	
-		    	if(e.getSource() == knappeListe[2] ) {
-		    		System.out.println("3");
-		    		knappeListe[2].setBackground(Color.blue);
-		    	} else  knappeListe[2].setBackground(Color.lightGray); 
-		    	
-		    	
-		    	if(e.getSource() == knappeListe[3] ) {
-		    		System.out.println("4");
-		    		knappeListe[3].setBackground(Color.blue);
-		    	} else  knappeListe[3].setBackground(Color.lightGray); 
-		    	
-		    	
-		    	if(e.getSource() == knappeListe[4] ) {
-		    		System.out.println("5");
-		    		knappeListe[4].setBackground(Color.blue);
-		    	} else  knappeListe[4].setBackground(Color.lightGray); 
-		    	
-		    	
-		    	if(e.getSource() == knappeListe[5] ) {
-		    		System.out.println("6");
-		    		knappeListe[5].setBackground(Color.blue);
-		    	} else  knappeListe[5].setBackground(Color.lightGray);  */
 
 		    }
 	  }
-	
+	  
+
 
 
 	
@@ -172,6 +141,7 @@ class toolBarKnapper {
 	
 
 	private JButton toolbarKnappen [];
+	Border emptyBorder = BorderFactory.createEmptyBorder();
 	
 
 	
@@ -180,19 +150,21 @@ class toolBarKnapper {
 		toolbarKnappen = new JButton[antallKnapper];
 
 	
-	toolbarKnappen[0] = new JButton("Lege Register");
-	toolbarKnappen[1] = new JButton("Pasient Register");
-	toolbarKnappen[2] = new JButton("Medisin Register");
-	toolbarKnappen[3] = new JButton("Resept Register");
-	toolbarKnappen[4] = new JButton("Statistikk");
-	toolbarKnappen[5] = new JButton("Informasjon");
+	toolbarKnappen[0] = new JButton(" Lege Register");
+	toolbarKnappen[1] = new JButton(" Pasient Register");
+	toolbarKnappen[2] = new JButton(" Medisin Register");
+	toolbarKnappen[3] = new JButton(" Resept Register");
+	toolbarKnappen[4] = new JButton(" Statistikk");
+	toolbarKnappen[5] = new JButton(" Informasjon");
 	
 
 	
 	//
 	for(JButton knappen: toolbarKnappen) {
-		knappen.setBackground((new Color(160,160,160)));
-
+		knappen.setBackground((new Color(128,128,128)));
+		knappen.setBorder(emptyBorder);
+		knappen.setFocusPainted(false);
+		
 
 		
 	}
