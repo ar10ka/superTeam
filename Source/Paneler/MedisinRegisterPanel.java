9c2e55e5bfbbf4f714c3cd79c764740027ee1dd5
superagurk <superagurken@gmail.com>
2014-05-07 13:08:01 +0200

    Paneler til DesignTest

---
+++ Source/Paneler/MedisinRegisterPanel.java
@@ -0,0 +1,341 @@
+/*
+
+Studentnr: s188097
+Navn: Ole Boee Andreassen
+
+
+Klasse: Dataingenioer
+
+*/
+
+package Program;
+import Program.Logg;
+import Program.Medisin;
+import Program.MedisinRegister;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.util.*;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.ParseException;
+import javax.swing.text.MaskFormatter;
+
+public class MedisinRegisterPanel extends JPanel
+{
+  JFormattedTextField medID;
+  JTextField 	medNavn;
+  JButton 		kNyMedisin, kSlettMedisin, kVisAlt, kVisMedisin;
+  JRadioButton 	radioA, radioB,radioC;
+  JTextArea		tekstområde,medInfo;
+  JSpinner medKategori;
+  
+  
+  MaskFormatter medIDformatter;
+  private MedisinRegister bibliotek;
+  private Lytter sensor;
+  public static final int _A = 1;
+  public static final int _B  = 2;
+  public static final int _C  = 3;
+  private Logg logg;
+	
+
+  public MedisinRegisterPanel()//konstruktør
+  {
+  
+    try{
+          this.medIDformatter = new MaskFormatter("U##U U##");
+    } 
+    catch (ParseException ex) {
+         ex.printStackTrace();
+      }
+    
+    
+    bibliotek  = new MedisinRegister();
+    logg = new Logg();
+    try
+    {
+      lastInnFil();
+    }
+
+    catch (IOException ex)
+    {
+      ex.printStackTrace();
+    }
+
+    radioA = new JRadioButton("A");
+    radioB = new JRadioButton("B");
+    radioC = new JRadioButton("C");
+    
+    ButtonGroup radioGruppe = new ButtonGroup();
+    radioGruppe.add(radioA);
+    radioGruppe.add(radioB);
+    radioGruppe.add(radioC);
+    
+    //MEDISIN INPUT FORMATTERING
+    medID = new JFormattedTextField(medIDformatter);
+    medID.setColumns(14);
+    
+    medNavn    = new JTextField(14);
+    
+    // KATEGORI RULLEVINDU
+    SpinnerListModel listModel = new SpinnerListModel(getKategori());
+    medKategori    = new JSpinner(listModel);
+    Dimension d = medKategori.getPreferredSize();
+    d.width = 100;
+    medKategori.setPreferredSize(d);
+    medKategori.setValue( getKategori().get(getKategori().size()-1));
+    
+    medInfo    = new JTextArea(15,30);
+    medInfo.setEditable(true);
+    JScrollPane rulle2 = new JScrollPane(medInfo);
+
+    kNyMedisin      = new JButton("Reg medisin");
+    kSlettMedisin   = new JButton("Slett medisin");
+    kVisMedisin     = new JButton("Vis medisin");
+    kVisAlt      = new JButton("Vis alle");
+    
+    tekstområde  = new JTextArea(15, 55);
+    tekstområde.setEditable(false);
+    JScrollPane rulle = new JScrollPane(tekstområde);
+    
+
+
+    setLayout( new FlowLayout() );
+    add(radioA);
+    add(radioB);
+    add(radioC);
+    add(new JLabel("Medisin ID:"));
+    add(medID);
+    add(new JLabel("Medisin navn:"));
+    add(medNavn);
+    add(new JLabel("Medisin kategori:"));
+    add(medKategori);
+    add(new JLabel("Medisin info:"));
+    add(rulle2);
+    add(kNyMedisin);
+    add(kSlettMedisin);
+    add(kVisMedisin);
+    add(kVisAlt);
+    add(rulle);
+
+
+    sensor = new Lytter();
+
+
+    kNyMedisin.addActionListener(sensor);
+
+    kSlettMedisin.addActionListener(sensor);
+
+    kVisMedisin.addActionListener(sensor);
+    kVisAlt.addActionListener(sensor);
+
+  }//slutt på konstruktør
+
+ public void lagreFil() throws IOException
+  {
+    try
+    {
+      ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("MedisinLagring.txt"));
+      out.writeObject(bibliotek);
+      System.out.println(logg.toString("Lagret!"));
+    }
+    catch (FileNotFoundException ex)
+    {
+      ex.printStackTrace();
+    }
+  }
+
+  private void lastInnFil() throws IOException
+  {
+    try
+    {
+      FileInputStream fileHandle = new FileInputStream("MedisinLagring.txt");
+      ObjectInputStream in = new ObjectInputStream(fileHandle);
+      bibliotek = (MedisinRegister) in.readObject();
+      System.out.println(logg.toString("Lastet inn!"));
+  
+    }
+    catch (FileNotFoundException ex)
+    {
+      System.out.println(logg.toString("Lager ny lagringsfil"));
+    }
+    catch (ClassNotFoundException ex)
+    {
+      System.out.println("feilen");
+      ex.printStackTrace();
+    }
+    catch (EOFException ex)
+    {
+      System.out.println(logg.toString("Ferdig lastet lagringsfil!"));
+    }
+  }
+
+  private class Lytter implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+      if (e.getSource() == kNyMedisin)
+      {
+          nyMedisin();
+      }
+      else if (e.getSource() == kSlettMedisin)
+      {
+        slettMedisin();
+      }
+      else if (e.getSource() == kVisMedisin)
+      {
+        visMedisin();
+      }
+      else if (e.getSource() == kVisAlt)
+      {
+        System.out.print(medKategori.getValue().toString());
+        visAlt();
+      }
+    }
+  }
+
+public static List<String> getKategori()
+{
+   String[] arr = 
+   {
+       "Hypnotikum","Antibiotika","Sedativum",
+       "Hemodialysekonsentrat","Glaukommiddel",
+       "Adrenergikum","Progestogen"
+   
+   };
+   List<String> k = Arrays.asList(arr);
+   Collections.sort(k); 
+   Collections.reverse(k);
+   return k;
+}
+  
+  
+public char aktivRadio()
+  {
+    if(radioA.isSelected())
+      return 'A';
+    if (radioB.isSelected())
+      return 'B';
+     if (radioC.isSelected())
+      return 'C';
+     
+         tekstområde.setText("Huk av for Firma eller Medisin!");
+         return 0;
+     
+        
+  }
+
+  public boolean feltFylt()
+  {
+      return aktivRadio() != 0 
+              && !medID.getText().equals("")
+              && !medNavn.getText().equals("")
+              && !medInfo.getText().equals("")
+              && !medKategori.getValue().toString().equals("");
+  }
+
+  public void nyMedisin()
+  {
+    try
+    {
+      String id = medID.getText();
+      String navn = medNavn.getText();
+      char rbStatus = aktivRadio();
+      String info = medInfo.getText();
+      String k = medKategori.getValue().toString();
+
+      if ( feltFylt() )
+      {
+        Medisin medisin = new Medisin(id, navn, info, k, rbStatus);
+        if (!bibliotek.finnes(medisin) )
+        {
+            System.out.print("Finnes medisin?");
+            bibliotek.settInnNy(medisin);
+            tekstområde.setText(logg.toString("Medisin lagt til"));
+        }
+        else
+            tekstområde.setText("Medisin allerede lagt til");
+      }
+      else
+        tekstområde.setText("Fyll ut alle feltene!");
+    }
+    catch (NumberFormatException e)
+    {
+      tekstområde.setText("Fyll ut alle feltene!");
+    }
+  }
+
+
+
+
+  public void slettMedisin()
+  {
+    try
+    {
+      String id = medID.getText();
+      String navn = medNavn.getText();
+      char rbStatus = aktivRadio();
+      String info = medInfo.getText();
+      String k = medKategori.getValue().toString();
+
+
+      if ( !id.equals("") )
+      {
+        Medisin medisin = new Medisin(id, navn, info, k, rbStatus);
+        if (bibliotek.finnes(medisin) )
+            {
+                if(bibliotek.fjern(medisin))
+                    tekstområde.setText(logg.toString("Medisin fjernet!"));
+            }
+        else
+           tekstområde.setText("Medisin finnes ikke!");
+
+      }
+      else
+        tekstområde.setText("Fyll ut medisin ID!");
+    }
+    catch (NumberFormatException e)
+    {
+      tekstområde.setText("Fyll ut medisin ID!");
+    }
+  }
+
+
+
+  public void visMedisin()
+  {
+     try
+    {
+      String id = medID.getText();
+      String navn = medNavn.getText();
+
+
+      if ( !id.equals(id) || !navn.equals(navn)  )
+      {
+        String temp = "Viser Medisin:\n" + bibliotek.finn(navn, id).toString();
+  	tekstområde.setText(logg.toString(temp));
+      }
+      else
+        tekstområde.setText("Fyll ut feltene!");
+    }
+    catch (NumberFormatException e)
+    {
+      tekstområde.setText("Fyll ut alle feltene!");
+    }
+  }
+
+  public void visAlt()
+  {
+    tekstområde.setText(logg.toString(bibliotek.toString()));
+  }
+}
+
---
+++ Source/Paneler/PasientRegisterPanel.java
@@ -0,0 +1,322 @@
+/*
+
+Studentnr: s188097
+Navn: Ole Boee Andreassen
+
+
+Klasse: Dataingenioer
+
+*/
+package Program;
+
+
+import Program.Logg;
+import Program.Pasient;
+import Program.PasientRegister;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.ParseException;
+import javax.swing.text.MaskFormatter;
+
+public class PasientRegisterPanel extends JPanel
+{
+  JFormattedTextField fNr;
+  JTextField 	 fNavn, eNavn, adresse;
+  JButton 		kNyPasient, kSlettPasient, kVisAlt, kVisPasient;
+  JRadioButton 	radioMann, radioKvinne;
+  JTextArea		tekstområde;
+  
+  MaskFormatter fNrformatter;
+
+  private PasientRegister bibliotek;
+  private char gender;
+  private Lytter sensor;
+  public static final int _PERSON = 1;
+  public static final int _FIRMA  = 2;
+  private final Logg logg;
+	
+
+  public PasientRegisterPanel()//konstruktør
+  {
+      try {
+          this.fNrformatter = new MaskFormatter("###### #####");
+      } catch (ParseException ex) {
+         ex.printStackTrace();
+      }
+
+    logg = new Logg();
+    bibliotek  = new PasientRegister();
+
+    try
+    {
+      lastInnFil();
+    }
+
+    catch (IOException ex)
+    {
+      ex.printStackTrace();
+    }
+
+    radioMann  = new JRadioButton("Mann");
+    radioKvinne   = new JRadioButton("Kvinne");
+    ButtonGroup radioGruppe = new ButtonGroup();
+    radioGruppe.add(radioMann);
+    radioGruppe.add(radioKvinne);
+
+    
+      //For å gjøre det mer oversiktelig i input så kommer det et blankt tegn etter de første 6 tallene
+    
+    fNr = new JFormattedTextField(fNrformatter);
+    fNr.setColumns(14);
+ 
+    fNavn    = new JTextField(14);
+    eNavn = new JTextField(14);
+    adresse = new JTextField(14);
+
+    kNyPasient      = new JButton("Reg pasient");
+    kSlettPasient   = new JButton("Slett pasient");
+    kVisPasient     = new JButton("Vis pasient");
+    kVisAlt      = new JButton("Vis alle");
+
+	tekstområde  = new JTextArea(15, 55);
+    tekstområde.setEditable(false);
+    JScrollPane rulle = new JScrollPane(tekstområde);
+
+
+
+    setLayout( new FlowLayout() );
+    add(radioMann);
+    add(radioKvinne);
+    add(new JLabel("Personnummer:"));
+    add(fNr);
+    add(new JLabel("Fornavn:"));
+    add(fNavn);
+    add(new JLabel("Etternavn:"));
+    add(eNavn);
+    add(new JLabel("Adresse:"));
+    add(adresse);
+
+
+    add(kNyPasient);
+    add(kSlettPasient);
+    add(kVisPasient);
+    add(kVisAlt);
+    add(rulle);
+
+
+
+
+
+    sensor = new Lytter();
+
+
+    kNyPasient.addActionListener(sensor);
+
+    kSlettPasient.addActionListener(sensor);
+
+    kVisPasient.addActionListener(sensor);
+    kVisAlt.addActionListener(sensor);
+
+  
+   
+  }
+  public void lagreFil() throws IOException
+  {
+    try
+    {
+      ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("PasientLagring.txt"));
+      out.writeObject(bibliotek);
+      System.out.println(logg.toString("Lagret!"));
+    }
+    catch (FileNotFoundException ex)
+    {
+      ex.printStackTrace();
+    }
+  }
+
+  private void lastInnFil() throws IOException
+  {
+    try
+    {
+      FileInputStream fileHandle = new FileInputStream("PasientLagring.txt");
+      ObjectInputStream in = new ObjectInputStream(fileHandle);
+      bibliotek = (PasientRegister) in.readObject();
+      System.out.println(logg.toString("Lastet inn!"));
+  
+    }
+    catch (FileNotFoundException ex)
+    {
+      System.out.println(logg.toString("Lager ny lagringsfil"));
+    }
+    catch (ClassNotFoundException ex)
+    {
+      System.out.println("feilen");
+      ex.printStackTrace();
+    }
+    catch (EOFException ex)
+    {
+      System.out.println(logg.toString("Ferdig lastet lagringsfil!"));
+    }
+  }
+
+  private class Lytter implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+      if (e.getSource() == kNyPasient)
+      {
+        //int rbStatus = aktivRadio();
+        //if(rbStatus == _PERSON)
+        //{
+          nyPasient();
+        //}
+        /*else if (rbStatus == _FIRMA)
+        {
+          nyFirma();
+        }*/
+      }
+      else if (e.getSource() == kSlettPasient)
+      {
+        slettPasient();
+      }
+      else if (e.getSource() == kVisPasient)
+      {
+        visPasient();
+      }
+      else if (e.getSource() == kVisAlt)
+      {
+        visAlt();
+      }
+    }
+  }
+
+  public void emptyInputField()
+  {
+      
+  }
+  
+public char aktivRadio()
+  {
+    if(radioMann.isSelected())
+      return 'M';
+    else if (radioKvinne.isSelected())
+      return 'F';
+
+    tekstområde.setText("Huk av for Mann eller Kvinne!");
+    return 0;
+  }
+
+
+  public void nyPasient()
+  {
+    try
+    {
+      String id = fNr.getText().replaceAll("\\s","");//får personnummer og fjerner blanke tegn
+      String fnavn = fNavn.getText();
+      String enavn = eNavn.getText();
+      char gen = aktivRadio();
+      String adr = adresse.getText();
+
+
+      if (!fnavn.equals("") && !enavn.equals("") && !id.equals("") && gen!=0 && !adr.equals("") )
+      {
+        if (!bibliotek.finnes(id))
+        {          
+            Pasient pasient = new Pasient(fnavn, enavn, id, gen,adr);
+            bibliotek.settInnNy(pasient);
+            tekstområde.setText(logg.toString("Pasient lagt til"));
+        }
+        else
+            tekstområde.setText("Pasienten er allerede registrert");
+      
+        
+      }
+        else
+         tekstområde.setText("Fyll ut alle feltene!");
+    }
+    catch (NumberFormatException e)
+    {
+      tekstområde.setText("Fyll ut alle feltene!");
+    }
+  }
+
+
+
+
+  public void slettPasient()
+  {
+    try
+    {
+      String id = fNr.getText().replaceAll("\\s","");
+
+
+
+      if (!id.equals("") )
+      {
+        if (bibliotek.finnes(id))
+        {
+            Pasient pasient = bibliotek.finn(id);
+        
+            if(bibliotek.fjern(pasient))
+               tekstområde.setText(logg.toString("Pasient fjernet!"));
+        }
+        else
+            tekstområde.setText("Pasienten finnes ikke i registeret");
+      }
+      else
+        tekstområde.setText("Skriv inn Personnummeret på pasienten du ønsker å fjerne!");
+    }
+    catch (NumberFormatException e)
+    {
+      tekstområde.setText("Fyll ut alle feltene!");
+    }
+  }
+
+
+
+  public void visPasient()
+  {
+     try
+    {
+      String id = fNr.getText().replaceAll("\\s","");//får personnummer og fjerner blanke tegn
+      String fnavn = fNavn.getText();
+      String enavn = eNavn.getText();
+
+
+
+      if (!id.equals("") )
+      {
+   
+        if (bibliotek.finnes(id))
+        {
+            String temp = "Viser pasient:\n" + bibliotek.finn(id).toString();
+  	  	 tekstområde.setText(logg.toString(temp)); 
+        }
+        else
+            tekstområde.setText("Pasienten finnes ikke i registeret");
+
+      }
+      else
+        tekstområde.setText("Skriv inn personnummer");
+    }
+    catch (NumberFormatException e)
+    {
+      tekstområde.setText("Fyll ut alle feltene!");
+    }
+  }
+
+  public void visAlt()
+  {
+      logg.toString("Viser alle pasienter");
+    tekstområde.setText(bibliotek.toString());
+  }
+}
+
